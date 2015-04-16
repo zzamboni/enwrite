fbee88b626fe69f2501f206e2341233005308d7f
Diego Zamboni <diego@zzamboni.org>
2015-04-15 23:11:36 -0700

    Functions to persist and read values in a config file

--- lib/util.rb
+++ lib/util.rb
@@ -1,3 +1,4 @@
+require 'yaml/store'
 
 def verbose(msg)
   puts msg if $enwrite_verbose
@@ -6,3 +7,21 @@ end
 def error(msg)
   $stderr.puts msg
 end
+
+def config_file
+  return "#{ENV['HOME']}/.enwrite.config"
+end
+
+def config_store
+  return YAML::Store.new(config_file())
+end
+
+def config(key, defval=nil)
+  store = config_store
+  return store.transaction { store.fetch(key, defval) }
+end
+
+def setconfig(key, val)
+  store = config_store
+  return store.transaction { store[key] = val }
+end  

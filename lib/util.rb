36bd79ef88f624491a0a1a4be4c6f8a2c51ffa8a
Diego Zamboni <diego@zzamboni.org>
2015-04-17 00:22:12 -0700

    Add color output in messages.

--- lib/util.rb
+++ lib/util.rb
@@ -1,16 +1,25 @@
 require 'yaml/store'
+require 'colorize'
+
+# Message output
 
 def verbose(msg)
-  puts "Enwrite [VERBOSE]: " + msg if $enwrite_verbose
+  puts ("Enwrite [VERBOSE]: " + msg).blue if $enwrite_verbose
 end
 
 def error(msg)
-  $stderr.puts "Enwrite [ERROR]: " + msg
+  $stderr.puts ("Enwrite [ERROR]: " + msg).red
 end
 
 def msg(msg)
-  puts "Enwrite [INFO]: " + msg
+  puts ("Enwrite [INFO]: " + msg).green
 end
+
+def warn(msg)
+  $stderr.puts ("Enwrite [WARN]: " + msg).light_yellow
+end
+
+# Config file storage
 
 def config_file
   return "#{ENV['HOME']}/.enwrite.config"
@@ -20,11 +29,13 @@ def config_store
   return YAML::Store.new(config_file())
 end
 
+# Get a persistent config value
 def config(key, defval=nil)
   store = config_store
   return store.transaction { store.fetch(key, defval) }
 end
 
+# Set a persistent config value
 def setconfig(key, val)
   store = config_store
   return store.transaction { store[key] = val }
